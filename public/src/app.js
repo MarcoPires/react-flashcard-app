e07d23c5e9509472d4fef057095992b7ecf905b9
MarcoPires <marcofspires@gmail.com>
2016-10-11 21:14:11 +0100

    adding sidebar component

--- public/src/app.js
+++ public/src/app.js
@@ -26,4 +26,28 @@ const App = (props) => {
 	);
 };
 
-ReactDOM.render(<App> Hello <strong>React</strong> </App>, document.getElementById('root'));
+
+const Sidebar = React.createClass({
+	render() {
+		let props = this.props;
+		
+		return(
+			<div className='sidebar'>
+				<h2>All Decks</h2>
+				<ul>
+				{props.decks.map((deck, i) =>
+					<li key={i}> {deck.name} </li>
+				)}
+				</ul>
+				{ props.addingDeck && <input ref='add' /> }
+			</div>
+		);
+	}
+});
+
+
+ReactDOM.render((
+	<App>
+		<Sidebar decks={[ {name: 'Deck 1'} ]} addingDeck={true} ></Sidebar>
+	</App>
+), document.getElementById('root'));
