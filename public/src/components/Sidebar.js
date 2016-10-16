ea48071c4ebdff3290d6865e207bc2da035bc58a
MarcoPires <marcofspires@gmail.com>
2016-10-16 16:14:23 +0100

    bind actions to events

--- public/src/components/Sidebar.js
+++ public/src/components/Sidebar.js
@@ -2,6 +2,7 @@
  * npm modules
  */
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 /**
  * Sidebar component
@@ -9,20 +10,40 @@ import React from 'react';
  * @return {String} HTML
  */
 const Sidebar = React.createClass({
+
+	componentDidUpdate(prevProps, prevState) {
+		 let el = ReactDOM.findDOMNode(this.refs.add);
+
+		 if(el) el.focus();
+	},
+
 	render() {
 		let props = this.props;
 		
 		return(
 			<div className='sidebar'>
 				<h2>All Decks</h2>
+				
+				<button onClick={ e => this.props.showAddDeck() }>
+					New Deck
+				</button>
+
 				<ul>
 				{props.decks.map((deck, i) =>
 					<li key={i}> {deck.name} </li>
 				)}
 				</ul>
-				{ props.addingDeck && <input ref='add' /> }
+
+				{ props.addingDeck && <input ref='add'  onKeyPress={this.createDeck}/> }
 			</div>
 		);
+	},
+	createDeck(evt) {
+		if (evt.which !== 13) return;
+
+		let name = ReactDOM.findDOMNode(this.refs.add).value;
+		this.props.addDeck(name);
+		this.props.hideAddDeck();
 	}
 });
 
